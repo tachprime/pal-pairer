b05dd3d54e0cb512eb910f3b0f3c0a66dc8e7e8f
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-16 23:49:08 -0500

    fixed some errors got css served correctly

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const apiRouter = require('./app/routing/apiRoutes');
 
 app.set('port', process.env.PORT || 8080);
 
-//endpoints to be handled by htmlRoutes.js
-app.use('/', htmlRouter);
+app.use(express.static(__dirname + '/app/public'));
+
 //endpoints to be handled by apiRoutes.js
 app.use('/', apiRouter);
+//endpoints to be handled by htmlRoutes.js
+app.use('/', htmlRouter);
 
 app.listen(app.get('port'), function () {
   console.log('Server running on %s', app.get('port'));
