77b1a3d82d33fde47bce52134d89c4a5a749bd54
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-01-20 19:27:44 -0500

    added mock data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 //todo fill out data with more mock info
 var friendsList = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "name": "PewDiePie",
+        "photo": "https://emptylighthouse-production.s3-us-west-2.amazonaws.com/s3fs-public/field/image/pdp_0.jpg",
+        "scores": [2, 2, 1, 1, 4, 3, 5, 1, 3, 5]
     },
     {
         "name": "Vin Diesel",
@@ -12,24 +12,24 @@ var friendsList = [
     },
     {
         "name": "Tony Stark",
-        "photo": "http://static.tumblr.com/yyz4w41/x3wlt3x5w/tumblr_lsq3zydcqn1qmlgfpo1_250.jpg",
+        "photo": "https://static.tumblr.com/yyz4w41/x3wlt3x5w/tumblr_lsq3zydcqn1qmlgfpo1_250.jpg",
         "scores": [1, 5, 1, 5, 1, 4, 4, 3, 4, 4]
     },
-    // {
-    //     "name": "",
-    //     "photo": "",
-    //     "scores": []
-    // },
-    // {
-    //     "name": "",
-    //     "photo": "",
-    //     "scores": []
-    // },
-    // {
-    //     "name": "",
-    //     "photo": "",
-    //     "scores": []
-    // },
+    {
+        "name": "James bond",
+        "photo": "https://s-media-cache-ak0.pinimg.com/originals/a5/5a/27/a55a27c602460b41027da3e4b2aa96a8.jpg",
+        "scores": [4, 4, 3, 3, 1, 2, 3, 5, 1, 3]
+    },
+    {
+        "name": "Ryan Reynolds",
+        "photo": "https://queerty-prodweb.s3.amazonaws.com/wp/docs/2016/02/49a4cec24601ed4e_ryanreynolds.xxxlarge_2.jpg",
+        "scores": [3, 2, 2, 2, 2, 1, 1, 2, 3, 5]
+    },
+    {
+        "name": "Neil Degrasse Tyson",
+        "photo": "https://www.thegreatcourses.com/media/professor/p/r/prof_neil_degrasse_tyson.1395425046.jpg",
+        "scores": [2, 1, 3, 5, 1, 1, 3, 3, 3, 2]
+    }
 ];
 
 module.exports = friendsList;
